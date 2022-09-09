75b77afb88d536292fa5dc41f3415428099f9685
ashirafkyaze <kyazzeashiraf91@gmail.com>
2022-09-09 14:28:30 -0700

    prints all possible different combinations of three digits

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -4,30 +4,40 @@
  * main - Entry point
  *
  * Description: print all possible different
- *           combinations of two digits.
+ *            combinations of two digits.
  *
  * Return: Always 0 (Success)
 */
 
 int main(void)
 {
-	int digit1 = 0, digit2;
+	int digit1 = 0;
+	int digit2, digit3;
 
 	while (digit1 <= 9)
 	{
 		digit2 = 0;
 		while (digit2 <= 9)
 		{
-			if (digit1 != digit2 && digit1 < digit2)
+			digit3 = 0;
+			while (digit3 <= 9)
 			{
-				putchar(digit1 + 48);
-				putchar(digit2 + 48);
-
-				if (digit1 + digit2 != 17)
+				if (digit1 != digit2 &&
+				    digit1 < digit2 &&
+				    digit2 != digit3 &&
+				    digit2 < digit3)
 				{
-					putchar(',');
-					putchar(' ');
+					putchar(digit1 + 48);
+					putchar(digit2 + 48);
+					putchar(digit3 + 48);
+
+					if (digit1 + digit2 + digit3 != 24)
+					{
+						putchar(',');
+						putchar(' ');
+					}
 				}
+				++digit3;
 			}
 			++digit2;
 		}
@@ -37,4 +47,3 @@ int main(void)
 
 	return (0);
 }
-
