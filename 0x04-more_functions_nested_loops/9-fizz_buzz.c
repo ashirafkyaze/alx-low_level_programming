60cfde5da1c74d9eed75fd27b9422f0b1b94ed42
ashirafkyaze <kyazzeashiraf91@gmail.com>
2022-09-16 09:38:10 -0700

    9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * main - prints Buzz each numbers of 3 and 5.
+ * Return: always 0.
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+			printf("Fizz");
+		else if (i % 5 == 0)
+			printf("Buzz");
+		else if ((i % 3 == 0) && (i % 5 == 0))
+			printf("FizzBuzz");
+		else
+			printf("%d", i);
+	}
+	printf("\n");
+	return (0);
+}
