035a28a6f5a219714734b7a6bb24b92b1cd5b55d
ashirafkyaze <kyazzeashiraf91@gmail.com>
2022-09-19 05:56:45 -0700

    swapping two numbers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - paramater that swaps the values of two integers.
+ * @a: input integer.
+ * @b: input integer.
+ * Return: no return  value.
+ */
+
+void swap_int(int *a, int *b)
+{
+	int *n;
+
+	*a = *n;
+	*b = *a;
+	*n = *b;
+}
