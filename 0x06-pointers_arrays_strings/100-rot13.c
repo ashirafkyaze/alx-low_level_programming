857aad2837a69fb948a385f6bee85220fcf87faa
ashirafkyaze <kyazzeashiraf91@gmail.com>
2022-09-22 01:55:48 -0700

    encoding using rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * rot13 - function that encodes a string.
+ * rot13 - function that encodes a string using rot13.
  * @s: input string.
- * Return:
+ * Return: the pointer to dest.
  */
 
 char *rot13(char *s)
