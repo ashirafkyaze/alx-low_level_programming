c58b16495f3ce59c1e126f5f4c3d07b956fb3274
ashirafkyaze <kyazzeashiraf91@gmail.com>
2022-09-30 08:30:04 -0700

    mutliply two numbers

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,29 +10,18 @@
 
 int main(int argc, char *argv[])
 {
-	int count, num1, num2, res;
-
-	count = num1 = num2 = res = 0;
-	if (argc > 0)
+	if (argc == 3)
 	{
-		while (count < argc)
-		{
-			if (argc == 3)
-			{
-				if (count == 1)
-					num1 = atoi(argv[count]);
-				else if (count == 2)
-					num2 = atoi(argv[count]);
-			}
-			else
-			{
-				printf("Error\n");
-				return (1);
-			}
-			res = (num1 * num2);
-			count++;
-		}
-		printf("%d\n", res);
+		int m;
+
+		m = (atoi(argv[1]) * atoi(argv[2]));
+		printf("%d\n", m);
 	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+
 	return (0);
 }
